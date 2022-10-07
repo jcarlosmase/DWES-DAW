2dc5b46412eefa1dc474fe4c240aa9676dd9d2ae
Juan C. Jiménez <jcarlosjmase@gmail.com>
2022-10-07 13:25:10 +0200

    Versión 07/10/2022

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+  <title>Página principal DWES-2DAW</title>
+  <meta charset="UTF-8" />
+ </head>
+ <body>
+  <h1>Página principal de la asignatura DWES-2DAW</h1>
+  <ul>
+	<li><a href="array1/index.php" > Ejercicio 1 de arrays </a></li>
+	<li><a href="array1/array2.php" > Ejercicio 2 de arrays </a></li>
+	<li><a href="array1/array3.php" > Ejercicio 3 de arrays </a></li>
+	<li><a href="array1/libros.php" > Ejercicio de arrays de libros </a></li>
+	<li><a href="notas/index.php" > Tarea notas alumnos </a></li>
+	<li>Formularios</li>
+	<ul>
+		<li><a href="formularios/index.php" >Ejemplo 1: Elemntos de formulario</a></li>
+	</ul>
+  </ul>
+ </body>
+</html>
